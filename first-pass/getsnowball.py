11b4b77aecea2d3de130406c2661cd4012ea4f29
seanc <sean.yhc@gmail.com>
2011-09-30 22:57:51 -0700

    Wrote a 2nd version of get_snowball. Changed the return type of get_friends & get_followers to Set.

--- first-pass/getsnowball.py
+++ first-pass/getsnowball.py
@@ -2,15 +2,15 @@ THROTTLE = 200
 
 def lookup(user_id):
     # return user's metadata information
-    return {'dummy': 'metadata', 'followers_count' : 4}
+    return {'screen_name': 'abc', 'followers_count' : 4}
 
 def get_friends(user_id):
     #return array of ids of users that user_id follows
-    return [1,2,3,4,5]
+    return {1,2,3,4,5}
 
 def get_followers(user_id):
     #return array of ids of users that follow user_id
-    return [5,6,7,8]
+    return {5,6,7,8}
 
 def get_snowball(start, hops, mutual=False):
     snowball = {} # dictionary of (id, metadata) pairs
@@ -45,4 +45,51 @@ def get_snowball(start, hops, mutual=False):
                 
     return snowball
 
-# added by sean
+
+
+def get_snowball_s(start, hops, mutual):
+    snowball_set = {}
+    to_crawl = set({start})
+    
+    for h in range(hops):
+        # get all friends and followers into queue
+        print "hop: ", h
+        all_related_users = set()
+        for user_id in to_crawl:
+            print "user_id: ", user_id
+            if user_id not in snowball_set:
+                metadata = lookup(user_id)
+                snowball_set[user_id] = metadata
+                
+                if metadata['followers_count'] < THROTTLE:
+                    #look into each user's f&f
+                    #related_users is a set of accounts that connect to user_id
+                    if mutual:
+                        #take intersection
+                        related_users = get_friends(user_id).intersection( get_followers(user_id) )
+                    else:
+                        #make union
+                        related_users = get_friends(user_id).union( get_followers(user_id) )
+                    
+                    print "related_users: ", related_users
+                
+                    #append all related users of each user_id in to_crawl into one big set
+                    all_related_users = all_related_users.union( related_users )
+                
+                else:
+                    print "followers exceed ", THROTTLE, ", skipped"
+            
+        #print "all_related_users: ", all_related_users
+        #clean up to_crawl, add all_related_users to to_crawl, except those already in snowball_set
+        to_crawl = all_related_users.difference(snowball_set)
+        print "to_crawl: ", to_crawl
+
+    return snowball_set
+
+print get_snowball_s(4, 2, False)
+
+
+
+
+
+

bd4b6f0712d57fb1d890d4de4a5776744e4a1b10
Sebastian Benthall <sbenthall@gmail.com>
2011-10-21 16:06:31 -0700

    use mallet to infer topics on sample-data using topic model

--- first-pass/mallet.sh
+++ first-pass/mallet.sh
@@ -4,4 +4,7 @@
 mallet-2.0.6/bin/mallet import-file --input sample-data/tweets.txt --output twitter-topic-input.mallet --keep-sequence --remove-stopwords
 
 #build topic model
-mallet-2.0.6/bin/mallet train-topics --input twitter-topic-input.mallet --num-topics 100 --output-state topic-state.gz --optimize-interval 100
+mallet-2.0.6/bin/mallet train-topics --input twitter-topic-input.mallet --inferencer-filename inferencer.mallet --num-topics 10 --output-state topic-state.gz --optimize-interval 150
+
+#test topic model by infering topics on data set
+mallet-2.0.6/bin/mallet infer-topics --input twitter-topic-input.mallet --inferencer inferencer.mallet --output-doc-topics inferred-topics.1

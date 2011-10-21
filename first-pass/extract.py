c908ded75eecdcae6209ffd168fd344e82f9d2e4
Sebastian Benthall <sbenthall@gmail.com>
2011-10-21 16:06:14 -0700

    extract from json

--- first-pass/extract.py
+++ first-pass/extract.py
@@ -8,6 +8,8 @@ LOG_PATH = "log/"
 DUMP_PATH = "sample-data/"
 DUMP_FILE = "sample-data/tweets.txt"
 
+JSON_LOG = True
+
 SNOWBALL_PATH = "snowball-47545000-3.json"
 if len(sys.argv) > 1:
     SNOWBALL_PATH = sys.argv[1]
@@ -20,32 +22,41 @@ def load_snowball():
     else:
         print "error"
 
-TWEET_PATTERN = "(\w*) (\d*)\nDate\: (.*)\n\n    (.*)\n"
-URL_REGEX = "http\://\S*"
-
-usernames = []
-
 def get_screen_names():
     snowball = load_snowball()
     pp(snowball)
     return [m['screen_name'] for m in snowball.values() if m.has_key('screen_name')]
 
 
-    if os.path.isfile("%s" % (NAME_PATH)):
-        names = open("%s" % (NAME_PATH),'r')
-        return names
-    else:
-        print "error"
-        return ""
-        
-def get_log(username):
-    
-    log_name = username + ".txt"    
-    log = open(LOG_PATH + log_name,'r')
-    return log    
-
+URL_REGEX = "http\://\S*"
 def clean_out_urls(tweet):
     return re.sub(URL_REGEX, '', tweet)
+
+
+def get_txt_log(username):    
+    log_path = "%s%s.txt"%(LOG_PATH,username) 
+    if os.path.isfile(log_name):
+        log = open(log_name,'r')
+        return log.read()    
+    else:
+        print "No log %s found, returning blank" % (log_name)
+        return ""
+
+TWEET_PATTERN = "(\w*) (\d*)\nDate\: (.*)\n\n    (.*)\n"
+def parse_txt_log(username):    
+    log = get_txt_log(username)
+    matches = re.findall(TWEET_PATTERN, log)
+
+    return [clean_out_urls(tweet) for (name,number,date,tweet) in matches]
+
+def parse_json_log(username):
+    log_name = "%s%s.json"%(LOG_PATH,username) 
+    if os.path.isfile(log_name):
+        log = json.loads(open(log_name,'r').read())
+        return [clean_out_urls(tweet['text']) for tweet in log]
+    else:
+        print "No log %s found, returning blank" % (log_name)
+        return ""
 
 def main():
     if not os.path.exists(DUMP_PATH):
@@ -57,18 +68,21 @@ def main():
     screen_names = get_screen_names()
     
     for screen_name in screen_names:
-        
-        log = get_log(screen_name)
-        matches = re.findall(TWEET_PATTERN, log)
 
-        for (name,number,date,tweet) in matches:
-            # clean out URLs to get rid of bogus 'words'
-            clean_tweet = clean_out_urls(tweet)
+        clean_tweets = []
+        if JSON_LOG:
+            clean_tweets = parse_json_log(screen_name)
+        else:
+            clean_tweets = parse_txt_log(screen_name)
 
+        for clean_tweet in clean_tweets:
             print(clean_tweet)
+            try:
+                tweet_file.write(u"twitter %s %s\n" % (screen_name, clean_tweet))
 
-            tweet_file.write("twitter %s %s\n" % (screen_name, clean_tweet))
-            tweet_file.flush()
+                tweet_file.flush()
+            except:
+                'Error: Exception writing this tweet'
 
     tweet_file.close()
 

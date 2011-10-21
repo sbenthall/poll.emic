f1a61417b352e2bbdb74f061d8592cd56eeb79ef
seanyhc <sean.yhc@gmail.com>
2011-10-21 16:10:30 -0700

    try commit

--- first-pass/extract.py
+++ first-pass/extract.py
@@ -2,17 +2,34 @@ import re
 import os
 import sys
 from pprint import pprint as pp
+import simplejson as json
 
-NAME_PATH = "names.txt"
-TWEET_PATTERN = "(\w*) (\d*)\nDate\: (.*)\n\n    (.*)\n"
 LOG_PATH = "log/"
 DUMP_PATH = "sample-data/"
 DUMP_FILE = "sample-data/tweets.txt"
+
+SNOWBALL_PATH = "snowball-47545000-3.json"
+if len(sys.argv) > 1:
+    SNOWBALL_PATH = sys.argv[1]
+
+def load_snowball():
+    if os.path.isfile("%s" % (SNOWBALL_PATH)):
+        snowball_file = open("%s" % (SNOWBALL_PATH),'r')
+        snowball = json.loads(snowball_file.read())
+        return snowball
+    else:
+        print "error"
+
+TWEET_PATTERN = "(\w*) (\d*)\nDate\: (.*)\n\n    (.*)\n"
 URL_REGEX = "http\://\S*"
 
 usernames = []
 
-def get_name():
+def get_screen_names():
+    snowball = load_snowball()
+    pp(snowball)
+    return [m['screen_name'] for m in snowball.values() if m.has_key('screen_name')]
+
 
     if os.path.isfile("%s" % (NAME_PATH)):
         names = open("%s" % (NAME_PATH),'r')
@@ -37,15 +54,12 @@ def main():
     # w+ create file if it doesn't exist, but overwrite if it does    
     tweet_file = open("%s" % (DUMP_FILE),'w+')
 
-    names = get_name()
-    for line in names:
-        usernames.append(line.strip())
-    print usernames
+    screen_names = get_screen_names()
     
-    for username in usernames:
+    for screen_name in screen_names:
         
-        log = get_log(username)
-        matches = re.findall(TWEET_PATTERN, log.read())
+        log = get_log(screen_name)
+        matches = re.findall(TWEET_PATTERN, log)
 
         for (name,number,date,tweet) in matches:
             # clean out URLs to get rid of bogus 'words'
@@ -53,7 +67,7 @@ def main():
 
             print(clean_tweet)
 
-            tweet_file.write("twitter %s %s\n" % (username, clean_tweet))
+            tweet_file.write("twitter %s %s\n" % (screen_name, clean_tweet))
             tweet_file.flush()
 
     tweet_file.close()

81961c1b2c5dcfecb4d47ede9dbb81a4e5309b18
Sebastian Benthall <sbenthall@gmail.com>
2011-10-28 15:07:54 -0700

    generate plot of entropy vs. followers

--- first-pass/correlate.py
+++ first-pass/correlate.py
@@ -4,7 +4,11 @@ import numpy
 from numpy import array,zeros,ones,dot
 from math import log
 import matplotlib.pyplot as pyplot
+import os
+import simplejson as json
 
+
+from pylab import axes, axis
 
 INFERRED_TOPICS_FILE = "inferred-topics.1"
 
@@ -16,6 +20,8 @@ NUM_TOPICS = 100
 
 TWEET_DATA_FILE = "sample-data/tweets.txt"
 TWEET_DATA_PATTERN = "twitter (\S*) (.*)\n"
+
+LOG_PATH = "log/"
 
 def parse_topics():
     ''' Returns an M by N array, where M is number of tweets, N is number of topics, and A[m,n] is the value of the m'th topic for the n'th tweet. '''
@@ -46,13 +52,22 @@ def parse_tweets():
 
     user_matrix = zeros((len(user_tweets),len(matches)))
 
+    indexed_usernames = []
     for user_index, (username,tweets) in enumerate(user_tweets.items()):
+        indexed_usernames.append(username)
         for tweet_index in tweets:
             user_matrix[user_index,tweet_index] = 1
 
 
-    return user_matrix
+    return user_matrix, indexed_usernames
 
+def get_followers(username):
+    # CODE DUPLICATED FROM extract.py NEEDS REFACTORING!!!!
+    log_name = "%s%s.json"%(LOG_PATH,username) 
+    if os.path.isfile(log_name):
+        log = json.loads(open(log_name,'r').read())
+        return log[0]['user']['followers_count']
+    
 def normalize(dist):
     total = sum(dist)
     return dist / total
@@ -65,7 +80,7 @@ def main():
     topic_matrix = parse_topics()
     print(topic_matrix.shape)
 
-    user_matrix = parse_tweets()
+    user_matrix, indexed_usernames = parse_tweets()
     print(user_matrix.shape)
 
     x_matrix = dot(user_matrix,topic_matrix)
@@ -77,12 +92,22 @@ def main():
 
     # the histogram of the data
     n, bins, patches = pyplot.hist(entropies, 50)
-    print(n, bins, patches)
+    print(n)
     pyplot.title("Entropy Histogram")
-    pyplot.savefig("histogram.png", format='png')
+    pyplot.savefig("entropy_histogram.png", format='png')
 
-    #pyplot.scatter(x_matrix[0,:],x_matrix[1,:])
+    followers = array([get_followers(username) for username in indexed_usernames])
+    print(followers)
+    n, bins, patches = pyplot.hist(followers,50)
+    pyplot.title("Followers Histogram")
+    pyplot.savefig("followers_histogram.png", format='png')
 
+    axes(yscale='log')
+    #should make the axis scale change with the _variance_ of data
+    axis([min(entropies)*0.99, max(entropies)*1.01, min(followers)*0.9, max(followers)*1.1])
+    pyplot.plot(entropies,followers,'bo')
+    pyplot.title('Entropy vs. Followers')
+    pyplot.savefig("ef_plot.png", format='png')
 
 if __name__ == "__main__":
     main()

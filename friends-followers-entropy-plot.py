5c24f477df1170d9be7abef821c50b89de500a1c
Sebastian Benthall <sbenthall@gmail.com>
2011-12-06 22:18:30 -0800

    fix 3D plotter to use fixed entropy script

--- friends-followers-entropy-plot.py
+++ friends-followers-entropy-plot.py
@@ -1,6 +1,6 @@
 import re
 from settings import *
-from utils import get_followers_count
+from utils import *
 from pprint import pprint as pp
 import numpy
 from numpy import array,zeros,ones,dot
@@ -13,14 +13,6 @@ from mpl_toolkits.mplot3d import Axes3D
 
 user_metadata_matrix = numpy.load('user_metadata_matrix.npy')
 user_topic_matrix = numpy.load('user_topic_matrix.npy')
-    
-def normalize(dist):
-    total = sum(dist)
-    return dist / total
-
-def entropy(dist):
-    return 0 - sum([p * log(p) for p in normalize(dist)])
-
 
 def main():
 

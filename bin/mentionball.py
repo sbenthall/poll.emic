2e819494c0e0e7df6c5991be293bdfcbcdc793c1
sb <sbenthall@gmail.com>
2013-12-25 01:10:06 -0500

    fix mentionball to use networkx and export to gexf

--- bin/mentionball.py
+++ bin/mentionball.py
@@ -5,11 +5,10 @@ from pprint import pprint as pp
 import networkx as nx
 import sys
 
-USER = "sbenthall"
-
-DATA = {}
-
 def get_mention_counts(user):
+
+    pp("Getting mention counts for %s" % user)
+
     mentions = [tweet['entities']['user_mentions']
                 for tweet in use_statuses_api(user)]
     
@@ -18,13 +17,47 @@ def get_mention_counts(user):
 
     return counts
 
+def get_mention_data(ego):
+    data = {}
 
-DATA[USER] = get_mention_counts(USER)
+    data[ego] = get_mention_counts(ego)
 
-for user in DATA[USER].keys():
-    DATA[user] = get_mention_counts(user)
+    for user in data[ego].keys():
+        data[user] = get_mention_counts(user)
 
-pp(DATA)
+    pp(data)
 
-file = open('mentionball.json','w')
-file.write(json.dumps(DATA))
+    return data
+
+def data_to_network(data):
+    G = nx.DiGraph()
+
+    for fromu in data.keys():
+        for tou in data[fromu].keys():
+            G.add_edge(fromu,tou,weight=data[fromu][tou])
+
+    return G
+
+def clean_ball(graph):
+    for user in graph.nodes():
+        if (len(graph.predecessors(user)) < 2 and
+            len(graph.neighbors(user)) < 1):
+            graph.remove_node(user)
+
+
+def main(ego):
+
+    data = get_mention_data(ego)
+
+    G = data_to_network(data)
+
+    clean_ball(G)
+
+    nx.write_gexf(G,"mentionball-%s.gexf" % ego)
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        main(sys.argv[1])
+    else:
+        print "Please include a screen name or user ID for ego as argument."
+

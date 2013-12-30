9cf46cb5c56b75ec022e4ee3c8a9b1be39452155
sb <sbenthall@gmail.com>
2013-12-29 23:31:22 -0500

    adding logging and abstracting out test user

--- tests/apiwrapper_test.py
+++ tests/apiwrapper_test.py
@@ -1,5 +1,8 @@
 from nose.tools import *
 from poll_emic.apiwrapper import *
+import logging
+
+test_user = 'twitter'
 
 def setup():
     print "SETUP!"
@@ -9,16 +12,16 @@ def teardown():
 
 @with_setup(setup,teardown)
 def lookup_test():
-    data = lookup('twitter')
+    data = lookup(test_user)
     assert isinstance(data,list), 'output is not a list'
     assert data[0]['screen_name'] == 'twitter', 'mismatched screen_name'
 
 @with_setup(setup,teardown)
 def get_friends_test():
-    data = get_friends('twitter')
+    data = get_friends(test_user)
     assert isinstance(data,set), 'output is not a set'
 
 @with_setup(setup,teardown)
 def get_followers_test():
-    data = get_followers('twitter')
+    data = get_followers(test_user)
     assert isinstance(data,set), 'output is not a set'

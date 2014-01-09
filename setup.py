64c9e333d2044c58749f584506a918b43984d12d
sb <sbenthall@gmail.com>
2014-01-09 17:25:22 -0500

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': '',
+    'author': 'Sebastian Benthall',
+    'url': 'https://github.com/sbenthall/poll.emic',
+    'download_url': 'https://github.com/sbenthall/poll.emic',
+    'author_email': 'sb@ischool.berkeley.edu',
+    'version': '0.1',
+    'install_requires': ['nose','simplejson','twitter','numpy','networkx'],
+    'packages': ['poll_emic'],
+    'scripts': [],
+    'name': 'poll.emic'
+}
+
+setup(**config)

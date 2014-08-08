8ff0343fae74fce6fe16e9ff1e5fc45f84c9fe65
BCE provisioner <bce@lists.berkeley.edu>
2014-08-07 22:05:07 -0700

    adding back dict_merge in utils.py, which I lost in the last merge

--- poll_emic/utils.py
+++ poll_emic/utils.py
@@ -57,3 +57,23 @@ def call_api(method,arguments):
         else:
             raise e
 
+
+# from
+# http://blog.impressiver.com/post/31434674390/deep-merge-multiple-python-dicts
+def dict_merge(target, *args):
+    # Merge multiple dicts
+    if len(args) > 1:
+        for obj in args:
+            dict_merge(target, obj)
+        return target
+
+    # Recursively merge dicts and set non-dict values
+    obj = args[0]
+    if not isinstance(obj, dict):
+        return obj
+    for k, v in obj.iteritems():
+        if k in target and isinstance(target[k], dict):
+            dict_merge(target[k], v)
+        else:
+            target[k] = deepcopy(v)
+    return target

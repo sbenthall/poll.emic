35d9cad82d266022d24b89b4d8e2f3288ea23ff6
sb <sbenthall@gmail.com>
2014-12-09 13:43:59 -0800

    adding crawlhastag executable script

---
+++ bin/crawlhashtag.py
@@ -0,0 +1,64 @@
+import datetime
+import poll_emic.cache as cache
+from poll_emic.authtwitter import oauth
+from pprint import pprint as pp
+import sys
+from twitter import TwitterStream
+
+def main(args):
+
+    users = []
+    tags = []
+
+    for arg in args:
+        # to do: deal with hashtags here
+        # proper regexes here please
+        if arg[0] is '@':
+            users.append(arg[1:])
+        else:
+            tags.append(arg)
+
+    #TODO: Convert usernames to ID numbers to make them valid 'follow' inputs
+
+    domain = 'stream.twitter.com'
+
+    twitter_stream = TwitterStream(auth=oauth,domain=domain)
+
+    iterator = twitter_stream.statuses.filter(track=','.join(tags))
+
+    #Will cache tweets in batches
+    save_rate = 100
+    
+    counter = 0
+
+    recent_tweets = []
+
+    for tweet in iterator:
+        counter = counter + 1
+
+        pp(tweet)
+        recent_tweets.append(tweet)
+
+        if counter % 20 == 0:
+            counter = 0
+
+            # prepare timestamp
+            now = datetime.datetime.now()
+            now = str(now)
+            now = now.replace(' ','-')
+            now = now.replace('.','-')
+
+            query = '+'.join(tags)
+            pp("Writing to %s-%s" % (query,now))
+
+            cache.write(domain,query,recent_tweets,timestamp=now)
+
+    #data = get_mentionball(ego,data,only_replies=True)
+    #nx.write_gexf(G,"mentionball-%s.gexf" % "+".join(args).replace('/','~'))
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        main(sys.argv[1:])
+    else:
+        print "Please include one or more arguments."
+
--- poll_emic/cache.py
+++ poll_emic/cache.py
@@ -7,28 +7,39 @@ from pprint import pprint as pp
 method_names = ["twitter.users.lookup",
                 "twitter.friends.ids",
                 "twitter.followers.ids",
-                "twitter.statuses.user_timeline"]
+                "twitter.statuses.user_timeline",
+                "stream.twitter.com"]
 
 config= ConfigParser.ConfigParser()
 config.read('config.cfg')
 
 CACHE_PATH = config.get('Settings','cachepath')
 
-for method_name in method_names:
+def setup_dir(method_name):
     path = os.path.join(CACHE_PATH,method_name)
     if not os.path.exists(path):
         os.makedirs(path)
 
-def file_path(method_name,user):
-    return os.path.join(CACHE_PATH,method_name,"%s.json" % user)
+for method_name in method_names:
+    setup_dir(method_name)
+    #path = os.path.join(CACHE_PATH,method_name)
+    #if not os.path.exists(path):
+    #    os.makedirs(path)
+
+
+def file_path(method_name,query,timestamp=None):
+    if timestamp is not None:
+        query = "%s-%s" % (query,timestamp)
+    return os.path.join(CACHE_PATH,method_name,"%s.json" % query)
 
 def has(method_name,user):
     return os.path.isfile(file_path(method_name,user))
 
-def write(method_name,user,data):
-    pp("Caching result of %s for %s" % (method_name,user))
+def write(method_name,query,data,timestamp=None):
+    pp("Caching result of %s for %s" % (method_name,query))
     pp(type(data))
-    cache_file = open(file_path(method_name,user), 'w')
+    path = file_path(method_name,query,timestamp=timestamp)
+    cache_file = open(path, 'w')
     cache_file.write(json.dumps(data))
 
 def clear(method_name,users):
